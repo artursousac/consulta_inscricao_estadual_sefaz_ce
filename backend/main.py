43d053d8d53c24d2a943892d89308ba48fc8f4e1
artursousac <artursousac@hotmail.com>
2025-05-10 12:48:05 -0300

    ajuste na nomenclatura do arquivo e importacao das funcoes

---
+++ backend/main.py
@@ -0,0 +1,3 @@
+from telegram import enviar_dados_telegram
+from sefaz import buscar_dados_sefaz
+
--- backend/sefaz.py
+++ backend/sefaz.py
@@ -3,5 +3,9 @@ import requests
 def buscar_dados_sefaz(cnpj):
     # Realiza o tratamento do CNPJ, retirando . - /
     cnpj = re.sub(r'[.\-\/\s]', '', cnpj)
-    print(cnpj)
+
+    # Define a URL da Chamada HTTP
     url = f"https://consultapublica.sefaz.ce.gov.br/sintegra/consultar?tipdocumento=2&numcnpjcgf={cnpj}"
+
+    
+
