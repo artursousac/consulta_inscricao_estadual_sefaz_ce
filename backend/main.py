ad92c3ff439455895aff5b4c010029eec5af51fe
artursousac <artursousac@hotmail.com>
2025-05-10 12:53:35 -0300

    adicao das variaveis de ambiente no arquivo main

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,8 @@
 from telegram import enviar_dados_telegram
 from sefaz import buscar_dados_sefaz
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+token_telegram = os.getenv("TOKEN_TELEGRAM")
+chat_id_telegram_1 = os.getenv("CHAT_ID")
